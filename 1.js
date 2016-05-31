b78896b5f5d60141c3b077a28d05d1c9fd782175
DeepikaVJ <djhanwar3@gmail.com>
2016-05-31 18:08:29 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,12 @@
+function show_confirm()
+{
+//var r=confirm("Press a button");
+if (true)
+  {
+  document.write("You pressed OK!");
+  }
+else
+  {
+  document.write("You pressed Cancel!");
+  }
+}
